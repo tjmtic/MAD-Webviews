691fa52841dbc5c4fe3c35d70c4bbbac09798dde
TM <tjmcard@gmail.com>
2024-01-16 23:29:48 -0800

    Show In MainActivity

--- app/src/main/java/com/abyxcz/mad_webviews/MainActivity.kt
+++ app/src/main/java/com/abyxcz/mad_webviews/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.abyxcz.mad_webviews
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.abyxcz.mad_webviews.ui.theme.MADWebviewsTheme
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+
+
+const val BASE_URL = "https://www.quadbuddy.com/privacy"
+const val SERVICE_URL = "webviews/"
+const val DEFAULT_TOKEN = "12345"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +35,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    //AccompanistWebClient(url = "${BASE_URL}${SERVICE_URL}${DEFAULT_TOKEN}")
+                    WebViewSample(url = "${BASE_URL}")
                 }
             }
         }
@@ -43,4 +57,62 @@ fun GreetingPreview() {
     MADWebviewsTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun AccompanistWebClient(
+    modifier: Modifier = Modifier,
+    url: String,
+) {
+
+    val webViewState = rememberWebViewState(url = url)
+    WebView(
+        modifier = modifier,
+        state = webViewState,
+        captureBackPresses = true,
+        onCreated = { it : WebView ->
+            it.settings.javaScriptEnabled = true
+        }
+    )
+}
+
+@Composable
+fun WebViewSample(
+    url: String,
+    webViewClient: WebViewClient = WebViewClient()
+) {
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                this.webViewClient = webViewClient
+                settings.javaScriptEnabled = true
+
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+                settings.setSupportZoom(true)
+
+                addJavascriptInterface(object : Any() {
+                    @JavascriptInterface
+                    fun onButtonClick(buttonId: String) {
+                        // Log the button click event
+                        Log.d("WebViewConsole", "Btn Clicked: $buttonId")
+
+                        post {
+                            when (buttonId) {
+
+                                "next-btn" -> {
+                                    //add your action in here
+                                    Log.d("WebViewConsole", "NEXT BUTTON CLICKED!")
+                                }
+                            }
+                        }
+                    }
+                }, "Android")
+            }
+        },
+        update = { webView ->
+            webView.loadUrl(url)
+        }
+    )
 }
